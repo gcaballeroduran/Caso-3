a37a498ee27a10e6ef6db56619d593bda3497b76
Gabriela Caballero <g.caballero@uniandes.edu.co>
2021-11-01 12:04:26 -0500

    Conección

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,4 +1,36 @@
+import java.io.DataOutputStream;
+import java.io.IOException;
 
-public class Cliente {
+// REFERENCIAS: https://www.programarya.com/Cursos-Avanzados/Java/Sockets
+public class Cliente extends Repetidor{
+	
+    public Cliente() throws IOException{
+    	
+    	super("cliente");
+    	
+    } 
+
+    public void startClient() //Método para iniciar el cliente
+    {
+        try
+        {
+            //Flujo de datos hacia el servidor
+            salidaServidor = new DataOutputStream(cs.getOutputStream());
+
+            //Se enviarán dos mensajes
+            for (int i = 0; i < 2; i++)
+            {
+                //Se escribe en el servidor usando su flujo de datos
+                salidaServidor.writeUTF("Este es el mensaje número " + (i+1) + "\n");
+            }
+
+            cs.close();//Fin de la conexión
+
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+        }
+    }
 
 }
--- src/Repetidor.java
+++ src/Repetidor.java
@@ -1,4 +1,29 @@
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 
+//REFERENCIAS: https://www.programarya.com/Cursos-Avanzados/Java/Sockets
 public class Repetidor {
 
+	private final int PUERTO = 1234; //Puerto para la conexión
+    private final String HOST = "localhost"; //Host para la conexión
+    protected String mensajeServidor; //Mensajes recibidos en el servidor
+    protected ServerSocket ss; //Socket del servidor
+    protected Socket cs; //Socket del cliente
+    protected DataOutputStream salidaServidor, salidaCliente; //Flujo de datos de salida
+
+    public Repetidor(String tipo) throws IOException 
+    {
+        if(tipo.equalsIgnoreCase("servidor"))
+        {
+            ss = new ServerSocket(PUERTO);//Se crea el socket para el servidor en puerto 1234
+            cs = new Socket(); //Socket para el cliente
+        }
+        else
+        {
+            cs = new Socket(HOST, PUERTO); //Socket para el cliente en localhost en puerto 1234
+        }
+    }	
+	
 }
